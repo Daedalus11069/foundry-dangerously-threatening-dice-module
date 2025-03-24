ed3787de811678de2e69ebef191b4995c937066c
daedalus11069 <daedalus969@gmail.com>
2025-03-24 04:55:15 -0700

    Fix code

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
-Hooks.once("init", () => {
-  Hooks.on("diceSoNiceRollStart", (messageId, context) => {
-    for (const dice of context.roll.dice) {
-      const match = dice.flavor.match(
-        /error:(?<error>\d+)\s*(threat:(?<threat>\d+))?|threat:(?<threat>\d+)\s*(error:(?<error>\d+))?|threat:(?<threat>\d+)|error:(?<error>\d+)/i
-      );
-      if (match !== null) {
-        let error = window.parseInt(match.groups.error, 10);
-        let threat = window.parseInt(match.groups.threat, 10);
-        if (error === 0 || isNaN(error)) {
-          error = 1;
-        }
-        if (threat === 0 || isNaN(threat)) {
-          threat = dice.faces;
-        }
-
-        if (dice.values[0] <= error) {
-          dice.options.sfx = {
-            specialEffect: "PlayAnimationParticleVortex"
-          };
-        } else if (dice.values[0] >= threat) {
-          dice.options.sfx = {
-            specialEffect: "PlayAnimationParticleSpiral"
-          };
-        }
+Hooks.on("diceSoNiceRollStart", (messageId, context) => {
+  for (const dice of context.roll.dice) {
+    const reg = /threat:(?<threat>\d+)|error:(?<error>\d+)/gi;
+    let match;
+    const groups = {};
+    while ((match = reg.exec(dice.flavor)) !== null) {
+      if (typeof match.groups.error !== "undefined") {
+        groups.error = match.groups.error;
+      }
+      if (typeof match.groups.threat !== "undefined") {
+        groups.threat = match.groups.threat;
       }
     }
-  });
+    let error = window.parseInt(groups.error, 10);
+    let threat = window.parseInt(groups.threat, 10);
+    if (error === 0 || isNaN(error)) {
+      error = 1;
+    }
+    if (threat === 0 || isNaN(threat)) {
+      threat = dice.faces;
+    }
+
+    if (dice.values[0] <= error) {
+      dice.options.sfx = {
+        specialEffect: "PlayAnimationParticleVortex"
+      };
+    } else if (dice.values[0] >= threat) {
+      dice.options.sfx = {
+        specialEffect: "PlayAnimationParticleSpiral"
+      };
+    }
+  }
 
   // Hooks.on("diceSoNiceRollComplete", messageId => {
   //   const message = game.messages.get(messageId);
